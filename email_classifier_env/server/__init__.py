0a9a2c018e711cc49a4d1368e23bf133106b9918
dineshboyala <dineshroyalb12@gmail.com>
2026-04-01 08:01:08 +0530

    Initial commit- Openenv project setup

---
+++ email_classifier_env/server/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""Email Classifier Env environment server components."""
+
+from .email_classifier_env_environment import EmailClassifierEnvironment
+
+__all__ = ["EmailClassifierEnvironment"]
