811178f3260a448eb919e15797777a2205c35eb4
dineshboyala <dineshroyalb12@gmail.com>
2026-04-08 00:41:33 +0530

    modified

--- email_classifier_env/server/app.py
+++ email_classifier_env/server/app.py
@@ -1,11 +1,7 @@
 from openenv.core.env_server.http_server import create_app
 
-try:
-    from ..models import EmailAction, EmailObservation
-    from .email_classifier_env_environment import EmailClassifierEnvironment
-except ModuleNotFoundError:
-    from models import EmailAction, EmailObservation
-    from server.email_classifier_env_environment import EmailClassifierEnvironment
+from models import EmailAction, EmailObservation
+from server.email_classifier_env_environment import EmailClassifierEnvironment
 
 
 app = create_app(
